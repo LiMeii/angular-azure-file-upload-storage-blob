c49d0a601d62de1a383c3bbf3b9cc0846c39b2c4
Li <mei90s.lee@gmail.com>
2022-10-24 10:52:23 +0800

    Create azure-file-storage.service.ts

---
+++ src/Web/src/app/store/azure-file-storage/services/azure-file-storage.service.ts
@@ -0,0 +1,79 @@
+import { Injectable, NgZone } from '@angular/core';
+import { BlobDeleteOptions, BlobServiceClient, ContainerClient } from '@azure/storage-blob';
+import { Store } from '@ngrx/store';
+import { environment } from 'src/environments/environment';
+import { uploadBlobProgressAction } from '../actions';
+import { UploadBlobProgress } from '../models/upload-blob-progress.model';
+
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class AzureFileStorageService {
+
+    private blobService =  new BlobServiceClient(`https://${environment.azure_storageAccountName}.blob.core.windows.net/?${environment.azure_sasToken}`);
+
+    private containerClient: ContainerClient = this.blobService.getContainerClient(environment.azure_storageContainerName);
+
+
+    constructor(private ngZone: NgZone, private store: Store) { }
+
+
+    public async getBlobsFromStorageContainer() {
+        const returnedBlobUrls: string[] = [];
+        for await (const blob of this.containerClient.listBlobsFlat()) {
+            returnedBlobUrls.push(`https://${environment.azure_storageAccountName}.blob.core.windows.net/${environment.azure_storageContainerName}/${blob.name}`);
+        }
+        return returnedBlobUrls;
+    }
+
+    public async uploadFileToBlob(file: File | null) {
+        if (!file) return;
+
+        // await this.containerClient.createIfNotExists({
+        //     access: 'container',
+        // });
+
+        const blockBlobClient = await this.containerClient.getBlockBlobClient(file?.name);
+
+        const uploadFileByteSize = file?.size;
+
+
+        const bufferSize = file?.size > 1024 * 1024 * 32 ? 1024 * 1024 * 4 : 1024 * 512;
+        const maxConcurrency = 8;
+
+        const options = {
+            blobHTTPHeaders: { blobContentType: file?.type },
+            maxSingleShotSize: bufferSize,
+            concurrency: maxConcurrency,
+            onProgress: (data: UploadBlobProgress) => this.onUploadProgress(data, uploadFileByteSize),
+        };
+
+        await blockBlobClient.uploadData(file, options);
+
+    };
+
+
+    public async deleteBlobIfItExists(blobName: string) {
+
+        const options: BlobDeleteOptions = {
+            deleteSnapshots: 'include'
+        };
+
+        const blockBlobClient = await this.containerClient.getBlockBlobClient(blobName);
+
+        await blockBlobClient.deleteIfExists(options);
+
+    }
+
+
+    public onUploadProgress(progress: UploadBlobProgress, fileSize: number) {
+        const percentage = Math.round(progress.loadedBytes / fileSize * 100);
+        this.ngZone.run(() => {
+            this.store.dispatch(uploadBlobProgressAction.uploadBlobProgress({ uploadBlobProgressVal: percentage }));
+        });
+
+    }
+
+}
