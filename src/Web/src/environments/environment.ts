f6b90303067c025cb109d0ca6bda1727b93b5c38
Li <mei90s.lee@gmail.com>
2022-10-24 14:24:39 +0800

    remove azure config

--- src/Web/src/environments/environment.ts
+++ src/Web/src/environments/environment.ts
@@ -1,20 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+
 
 export const environment = {
   production: false,
-  azure_storageContainerName: 'uploadfiles',
-  azure_sasToken: 'sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2022-10-30T17:36:42Z&st=2022-10-20T09:36:42Z&spr=https&sig=jS4WrjYwGeuLsch1yrceANHfoRzRSsjTFJMZp%2BXGlu0%3D',
-  azure_storageAccountName: 'azfileuploaddemo'
+  azure_storageContainerName: '', // Put your own Azure storage container name here
+  azure_sasToken: '', //Put your own Azure storage sasToken here
+  azure_storageAccountName: '' //Put your own Azure storage account name here
 
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
